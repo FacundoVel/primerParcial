04b67a4e21c6f67a7ae0265ba9c455381bcd2a00
FacundoVel <facundo.vel99@gmail.com>
2025-07-29 00:11:37 -0300

    Se agrega archivo parcial.c para incorporar las listas

---
+++ PARCIAL.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <funciones.h>
+
+int main()
+{
+    Paciente *cardio = PacientesCardio();
+    Paciente *pediatria = PacientesPediatria();
+    Paciente *traumato = PacientesTraumato();
+
+
+    return 0;
+}
