59b952620e1a915f82d9ccb4b45abd05efba4667
FacundoVel <facundo.vel99@gmail.com>
2025-07-29 00:32:02 -0300

    Se crea funcion para unificar las listas

--- PARCIAL.c
+++ PARCIAL.c
@@ -2,6 +2,9 @@
 
 NodoPaciente *CrearListaVacia();
 NodoPaciente *CrearNodo(Paciente p);
+void InsertarAlFinal(NodoPaciente **inicio, Paciente p);
+void GenerarArchivoCentral(NodoPaciente **archivo, Paciente *cardio, Paciente *pedia, Paciente *trauma, int cant, int idInicial);
+
 
 int main()
 {
@@ -43,5 +46,25 @@ void InsertarAlFinal(NodoPaciente **inicio, Paciente p)
           aux = aux->siguiente;
         }
         aux->siguiente = nuevo;
+    }
+}
+
+void GenerarArchivoCentral(NodoPaciente **archivo, Paciente *cardio, Paciente *pedia, Paciente *trauma, int cant, int idInicial)
+{
+    int id = idInicial;
+
+    for(int i = 0; i < cant; i++)
+    {
+        Paciente p1 = cardio[i];
+        p1.IDPaciente = id++;
+        InsertarAlFInal(archivo, p1);
+
+        Paciente p2 = pedia[i];
+        p2.IDPaciente = id++;
+        InsertarAlFInal(archivo, p2);
+
+        Paciente p3 = trauma[i];
+        p3.IDPaciente = id++;
+        InsertarAlFInal(archivo, p3);
     }
 }
